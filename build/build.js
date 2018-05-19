6f0037be1a7043ed9b1a2d050f6361d205f95db3
Jordan Santell <jordan@jsantell.com>
2018-05-19 12:46:07 -0700

    Switch to manual build instead of metalsmith

--- build/build.js
+++ build/build.js
@@ -1,42 +1,51 @@
 #!/bin/node
 
 const path = require('path');
-const Metalsmith  = require('metalsmith');
-const layouts     = require('metalsmith-layouts');
-const dateFormatter = require('metalsmith-date-formatter');
-const markdown    = require('metalsmith-markdown');
-const perma = require('metalsmith-perma');
+const fs = require('fs').promises;
+const util = require('util');
+const yaml = require('js-yaml');
+const Handlebars = require('handlebars');
+const moment = require('moment');
 
-function build (type) {
-  const isEmail = type === 'email';
-  const destination = isEmail ? './emails' : './public';
-  const layout = isEmail ? 'email.hbs' : 'issue.hbs';
+const CONTENT_DIR = path.join(__dirname, '..', 'content');
+const EMAIL_OUTPUT_DIR = path.join(__dirname, '..', 'emails');
+const ISSUE_OUTPUT_DIR = path.join(__dirname, '..', 'public', 'issues');
+const TEMPLATE_DIR = path.join(__dirname, '..', 'layouts');
 
-  return new Promise((resolve, reject) => {
-    Metalsmith(path.join(__dirname, '..'))
-      .metadata({
-        sitename: "My Static Site & Blog",
-        siteurl: "http://example.com/",
-        description: "It's about saying »Hello« to the world.",
-      })
-      .source('./content')
-      .destination(destination)
-      .clean(false)
-      .use(dateFormatter({
-        dates: [{
-          key: 'date',
-          format: 'MMMM DD, YYYY'
-        }]
-      }))
-      .use(markdown())
-      .use(isEmail ? function(){ return function(){} } : perma({
-        pattern: 'issues/:issue'
-      }))
-      .use(layouts({
-        default: layout,
-      }))
-      .build(err => err ? reject(err) : resolve());
-  });
+const issueTemplate = Handlebars.compile(path.join(TEMPLATE_DIR, 'issue.hbs'));
+
+async function build (type) {
+  const emailTemplateSrc = await fs.readFile(path.join(TEMPLATE_DIR, 'email.hbs'), 'utf8');
+  const issueTemplateSrc = await fs.readFile(path.join(TEMPLATE_DIR, 'issue.hbs'), 'utf8');
+  const emailTemplate = Handlebars.compile(emailTemplateSrc);
+  const issueTemplate = Handlebars.compile(issueTemplateSrc);
+
+  const files = await fs.readdir(CONTENT_DIR);
+
+  for (let file of files) {
+    // Skip files that are not of format `###.yaml`
+    if (!/^\d\d\d\.yaml$/.test(file)) {
+      continue;
+    }
+    const pathToFile = path.join(CONTENT_DIR, file);
+    const yamlContents = await fs.readFile(pathToFile, 'utf8');
+    const meta = yaml.safeLoad(yamlContents);
+    const issue = file.match(/\d\d\d/)[0];
+
+    // Update meta with some more values for our templates
+    meta.issue = issue;
+    meta.permalink = `https://immersivewebweekly.com/issues/${meta.issue}`;
+    meta.date = moment(meta.date).format('MMMM DD, YYYY');
+
+    const emailMarkup = emailTemplate(meta);
+    const issueMarkup = issueTemplate(meta);
+
+    await fs.writeFile(path.join(EMAIL_OUTPUT_DIR, `${meta.issue}.html`), emailMarkup);
+    try {
+      await fs.mkdir(path.join(ISSUE_OUTPUT_DIR, meta.issue));
+    } catch (e) {}
+    await fs.writeFile(path.join(ISSUE_OUTPUT_DIR, meta.issue, 'index.html'), issueMarkup);
+  }
 }
 
-build('archive').then(() => build('email')).then(() => console.log('done')).catch(console.error);
+build();
